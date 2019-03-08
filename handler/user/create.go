3407fbf8f0916a3838cb7db6a13893e7aeb685ea
likun_gong <likun_gong@kingdee.com>
2019-03-08 17:27:35 +0800

    Set response function

--- handler/user/create.go
+++ handler/user/create.go
@@ -2,39 +2,44 @@ package user
 
 import (
 	"fmt"
+	. "github.com/Tizeen/go-restful-example/handler"
 	"github.com/Tizeen/go-restful-example/pkg/errno"
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
-	"net/http"
 )
 
 func Create(c *gin.Context) {
-	var r struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
 
-	var err error
+	var r CreateRequest
+
 	if err := c.Bind(&r); err != nil {
-		c.JSON(http.StatusOK, gin.H{"error": errno.ErrBind})
+		SendResponse(c, errno.ErrBind, nil)
 		return
 	}
 
-	log.Debug(r.Username + r.Password)
+	admin2 := c.Param("username")
+	log.Infof("URL name: %s", admin2)
+
+	desc := c.Query("desc")
+	log.Infof("URL key param desc: %s", desc)
+
+	contentType := c.GetHeader("Content-Type")
+	log.Infof("Header Content-Type: %s", contentType)
+
 	log.Debugf("username is: [%s], password is [%s]", r.Username, r.Password)
 	if r.Username == "" {
-		err = errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx.xx")).Add("This is add message.")
-		log.Errorf(err, "Get an error")
-	}
-
-	if errno.IsErrUserNotFound(err) {
-		log.Debug("err type is ErrUserNotFound")
+		SendResponse(c, errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")), nil)
+		return
 	}
 
 	if r.Password == "" {
-		err = fmt.Errorf("password is empty")
+		SendResponse(c, fmt.Errorf("password is Empty"), nil)
 	}
 
-	code, message := errno.DecodeErr(err)
-	c.JSON(http.StatusOK, gin.H{"code": code, "message": message})
+	rsp := CreateResponse{
+		Username: r.Username,
+	}
+
+	SendResponse(c, nil, rsp)
+
 }
